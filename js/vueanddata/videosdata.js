98217714fffbc007d815b82cf54b46eb84315185
helloweiqiang <zidianchefang@126.com>
2017-01-30 16:14:59 +0800

    完成新闻资讯模块，新闻主题采用视频作为主体，对首页的文字篇幅进行缩减，对儿童报道页面介绍进行增加，去除文字版的国外新闻，新闻资讯增加新闻栏目行数

---
+++ js/vueanddata/videosdata.js
@@ -0,0 +1,48 @@
+var app = new Vue({
+	el: '#video',
+	data: {
+		video1: {
+			title: "20151009杭州",
+			main1:"车祸现场 环卫工人交警轮流为伤者撑伞",
+			href:"http://player.youku.com/embed/XMTM1NjAwMjY0OA==",
+			href1:"video_pages/video1.html",
+			img:"images/videosimg/huanwei.jpg"
+		},
+		video2:{
+			title:"宝马车恶意变道",
+			main1:"江苏扬州_ 宝马车恶意变道致严重车祸 警方立案侦查",
+			href:"http://player.youku.com/embed/XMTg5NjY2MjA2MA==",
+			href1:"video_pages/video2.html",
+			img:"images/videosimg/eyi.jpeg"
+		},
+		video3:{
+			title:"奔驰车祸现场",
+			main1:"白色奔驰一头撞向泔水车 司机傻眼逃亡",
+			href:"http://player.youku.com/embed/XMjQ3NzM1ODY4MA==f",
+			href1:"video_pages/video3.html",
+			img:"images/videosimg/benchi.png"
+		},
+		video4:{
+			title:"兰博基尼大牛追尾货车",
+			main1:"兰博基尼大牛追尾货车面目全非Aventador埃文塔多车祸 记录仪实拍各种豪车超跑交通事故合集 面包撞上宾利被吓哭 保时捷撞行人被碰瓷",
+			href:"http://player.youku.com/embed/XMTc4NDc5NTU3Mg==",
+			href1:"video_pages/video4.html",
+			img:"images/videosimg/lanbo.jpeg"
+		},
+		video5:{
+			title:"奥迪R8超速失控撞上大客车",
+			main1:" 行车记录仪实拍各种豪车超跑交通事故合集 法拉利458超车失败撞到路人 保时捷911车祸撞电动车被碰瓷 面包车撞上劳斯莱斯",
+			href:"http://player.youku.com/embed/XMTc4MDU1OTMyMA==",
+			href1:"video_pages/video5.html",
+			img:"images/videosimg/aodi.jpeg"
+		},
+		video6:{
+			title:"中原晨报20150414",
+			main1:"中原晨报20150414两豪车北京大屯路发生严重交通事故",
+			href:"http://player.youku.com/embed/XOTMzNDMxNDU2",
+			href1:"video_pages/video6.html",
+			img:"images/videosimg/haoche.jpeg"
+		},
+		
+	}
+})
